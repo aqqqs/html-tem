62517df5797f139e00bff4da0cea84efb5502eb3
Tagir Gabbasov <qizilqara@yandex.ru>
2021-01-08 16:34:57 +0500

    console.group +

---
+++ .js/consoleGroup.js
@@ -0,0 +1,18 @@
+function consoleGr () {
+	console.group ('Level')
+	console.log ('Hello')
+	console.group ('New Level')
+	console.log ('Nihao')
+	console.group ('Next Level')
+	console.log ('Hola')
+	console.group ('More Level')
+	console.log ('Yahho Hisashiburi')
+	console.log ('Jaa')
+	console.groupEnd ()
+	console.log ('Hasta luego')
+	console.groupEnd ()
+	console.log ('Zai Zen')
+	console.groupEnd ()
+	console.log ('Bye')
+	console.groupEnd ()
+}
